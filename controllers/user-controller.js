3dceab3df90d62eb2ed01504a27bbeda213cff98
Chris Short <twitchshort@gmail.com>
2020-07-12 08:01:57 -0500

    Added basic user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,10 @@
-const { User } = require('./models');
+const { User } = require('../models');
 
 const userController = {
     // get all da users
     getAllUser(req, res) {
         User.find({})
-        .then(dbUserData => res.json(dbPizzaData))
+        .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -27,6 +27,39 @@ const userController = {
             res.status(400).json(err)
         });
     },
+
+    // Create user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // Delete User
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then (dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with that id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));            
+    }
 };
 
 module.exports = userController;
